65e025a5e4f59c1e64964b7a0e20ba6097975d49
AntRod18 <antrod1895@gmail.com>
2021-02-17 13:34:47 -0500

    commit tree e54a389edf829ddeb5b34fe7ba155bff43df1fac

---
+++ lib/artist.rb
@@ -0,0 +1,39 @@
+class Artist
+    attr_accessor :name, :songs
+
+    @@all = []
+
+    def initialize(name)
+        @name = name
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def songs
+        Song.all.select {|song| song.artist == self}
+    end
+
+    def add_song(song)
+        song.artist = self
+    end
+
+    def self.find_or_create_by_name(artist_name)
+        search = self.all.find {|artist| artist.name == artist_name}
+        if search 
+            return search
+        else
+            new_artist = self.new(artist_name)
+            new_artist
+        end
+    end
+
+    def print_songs
+        songs.each {|song| puts song.name}
+    end
+        
+        
+
+end
